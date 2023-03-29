1bbabbe488062f13d99e58532ee09f56a53be6ef
IWANNIS TRIANTAFYLLOPOULOS <sdi2000193@linux22.di.uoa.gr>
2023-03-29 15:15:50 +0300

    Code refactoring. Added README.md

---
+++ MPI/README.md
@@ -0,0 +1,48 @@
+# Parallel programing with MPI
+In this project we experiment with two different kinds of problems
+and face different challenges. It is highly recommended to run the
+experiments and see the results for yourself for a better understanding. 
+This project was made for my Parallel Systems course at National
+Kapodistrian University of Athens, Department of Informatics and
+Telecommunications.
+
+## Pi calculation
+
+### General
+Here we use the Monte Carlo method to find an approximation of pi. Check
+this link for the explanation of the algorithm:
+https://www.geeksforgeeks.org/estimating-value-pi-using-monte-carlo/
+
+First the process 0 executes the serial algorithm and then we proceed
+to the parallel one using all of the processes. To compile run "make pi"
+and to execute "mpiexec -n <num_of_processes> ./bin/pi_comp <throw_count>.
+With the proper configuration we could use multiple computers to execute
+our program, however for the sake of simplicity we execute locally.
+You can also run the pi_script.py to perform an experiment.
+Our program will be executed with different parameters multiple times each.
+In that way we will get the average values of the experiment each time since
+sometimes individual execution might return unrealistic results
+caused by external factors. Feel free to mess with the constants inside
+the script.
+
+## Matrix-vector multiplication
+
+### General
+We will multiply a square matrix and a vector but not with the traditional way.
+Someone would expect to do it as inside the mpi_mat_vect_time.c program,
+splitting the matrix in block rows from the book "Introduction to Parallel
+Programing" of Pachecco. Here we will do the multiplication by splitting
+the matrix in block columns. To build the executable run "make mat" and to
+execute it run "mpiexec -n <num_of_processes> ./bin/mpi_mat_vect <matrix_dim>"
+or "make runmat" to execute with arguments defined inside the Makefile.
+
+We will not explain exactly how the multiplication is
+happening but here is an example to help you understand.
+
+     | 1 1 6 | | 2 |     | 1 |     | 1 |     | 6 |   | 7 |
+Ax = | 3 0 3 | | 5 | = 2 | 3 | + 5 | 0 | + 0 | 3 | = | 6 |
+     | 1 1 4 | | 0 |     | 1 |     | 1 |     | 4 |   | 7 |
+
+As we can see, each process can get some columns of A, a part of x and
+then calculate a fraction of the final x element. After that, process 0
+will collect these fractions, add them up and get the final result.
--- MPI/src/pi_comp.c
+++ MPI/src/pi_comp.c
@@ -10,59 +10,78 @@ int main(int argc, char** argv){
     double start, finish, MPI_time, serial_time, local_MPI_time = 0;
     long double pi_serial, pi_MPI;
     int my_rank, num_of_proc;
-    throw_count = atoi(argv[1]);
     
+    // Initialise MPI, get neccessary info
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &num_of_proc);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+    // For illustrative purposes we get throw count
+    // from stdin so we can use MPI_Bcast later
+    // We could simply get it as a command line argument
+    /*
+    if (my_rank == 0) {
+        printf("Enter number of throws: ");
+        scanf("%lld", &throw_count);
+    }
     MPI_Bcast(&throw_count, 1, MPI_LONG, 0, MPI_COMM_WORLD);
-
-    if(my_rank == 0){
+    */
+    
+    throw_count = atoi(argv[1]);
+    
+    // Process one will execute the serial algorithm
+    if (my_rank == 0) {
         start = MPI_Wtime();
-        for(long long i = 0; i < throw_count; i++){
-        unsigned int seed = time(NULL) + i + my_rank;
-        double x_rand = get_rand(&seed);
-        double y_rand = get_rand(&seed);
-        double dist_square = x_rand * x_rand + y_rand * y_rand;
-        if(dist_square <= 1){
-            circle_hits++;
-        }
-        }
+        for(long long i = 0; i < throw_count; i++) {
+            unsigned int seed = time(NULL) + i + my_rank;
+            double x_rand = get_rand(&seed);
+            double y_rand = get_rand(&seed);
+            double dist_square = x_rand * x_rand + y_rand * y_rand;
+            
+            if (dist_square <= 1) {
+                circle_hits++;
+            }
+    }
         finish = MPI_Wtime();
         serial_time = finish - start;
         pi_serial = 4 * circle_hits / ((double) throw_count);
         printf("Time for serial estimation: %f | Pi serial estimation: %Lf\n", serial_time, pi_serial);
 
     }
+
     start = 0;
     finish = 0;
     circle_hits = 0;
     local_throw_count = throw_count / num_of_proc;
    
+    // Wait for all process to arrive here
     MPI_Barrier(MPI_COMM_WORLD);
+    
     start = MPI_Wtime();
-
-     for(long long i = 0; i < local_throw_count; i++){
+    for (long long i = 0; i < local_throw_count; i++) {
         unsigned int seed = time(NULL) + i + my_rank;
         double x_rand = get_rand(&seed);
         double y_rand = get_rand(&seed);
         double dist_square = x_rand * x_rand + y_rand * y_rand;
-        if(dist_square <= 1){
+        
+        if (dist_square <= 1) {
             local_circle_hits++;
         }
     }
 
+    // Process 0 collects results from other processes and add them together
+    MPI_Reduce(&local_circle_hits, &circle_hits, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
     finish = MPI_Wtime();
     local_MPI_time = finish - start;
-    
-    MPI_Reduce(&local_circle_hits, &circle_hits, 1, MPI_LONG_LONG_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&local_MPI_time, &MPI_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    if(my_rank == 0){
+    if (my_rank == 0) {
+        // Total time is process 0 local time since it's the one collecting the results
+        MPI_time = local_MPI_time;
         pi_MPI = 4 * circle_hits / ((double) throw_count);
         printf("Time for parallel estimation: %f | Pi parallel estimation: %Lf\n", MPI_time, pi_MPI);
     }
+
     MPI_Finalize();
     return 0;
 }
